b8cc5b75b026c4e682083f5fdb5cf41179f6cde2
Erik Sapir <erik.sapir@wilcohq.com>
2022-09-05 09:32:07 +0300

    ga

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -5,38 +5,38 @@ import { connect } from "react-redux";
 import { UPDATE_TITLE_SEARCH_TERM } from "../../constants/actionTypes";
 
 const Banner = (props) => {
-    const onChangeHandler = (event) => {
-        event.preventDefault();
-        const { value } = event.target;
-        props.dispatch({
-            type: UPDATE_TITLE_SEARCH_TERM,
-            payload: { titleSearchTerm: value },
-        });
-    };
+  const onChangeHandler = (event) => {
+    event.preventDefault();
+    const { value } = event.target;
+    props.dispatch({
+      type: UPDATE_TITLE_SEARCH_TERM,
+      payload: { titleSearchTerm: value },
+    });
+  };
 
-    return (
-        <div className="banner text-white">
-            <div className="container p-4 text-center">
-                <img src={logo} alt="banner" />
-                <div className="d-flex flex-row justify-content-center align-items-baseline">
+  return (
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div className="d-flex flex-row justify-content-center align-items-baseline">
           <span id="get-part" className="">
             A place to get
           </span>
-                    <form className="col-7">
-                        <fieldset className="form-group">
-                            <input
-                                className="form-control"
-                                id="search-box"
-                                type="search"
-                                placeholder="What is it that you truly desire?"
-                                onChange={onChangeHandler}
-                            />
-                        </fieldset>
-                    </form>
-                    <span className=""> the cool stuff.</span>
-                </div>
-            </div>
+          <form className="col-7">
+            <fieldset className="form-group">
+              <input
+                className="form-control"
+                id="search-box"
+                type="search"
+                placeholder="What is it that you truly desire?"
+                onChange={onChangeHandler}
+              />
+            </fieldset>
+          </form>
+          <span className=""> the cool stuff.</span>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 export default connect()(Banner);
--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -3,40 +3,40 @@ import ListPagination from "./ListPagination";
 import React from "react";
 
 const ItemList = (props) => {
-    let items = props.items;
+  let items = props.items;
 
-    if (!props.items) {
-        return <div className="py-4">Loading...</div>;
-    }
-    if (props.items.length === 0) {
-        return <div className="py-4 no-items">No items are here... yet.</div>;
-    }
+  if (!props.items) {
+    return <div className="py-4">Loading...</div>;
+  }
+  if (props.items.length === 0) {
+    return <div className="py-4 no-items">No items are here... yet.</div>;
+  }
 
-    if (props.titleSearchTerm && props.titleSearchTerm.length >= 3) {
-        items = items.filter((item) =>
-            item.title
-                .toLowerCase()
-                .includes(props.titleSearchTerm.toLowerCase().trim())
-        );
-    }
-
-    return (
-        <div className="container py-2">
-            <div className="row">
-                {items.map((item) => {
-                    return (
-                        <div className="col-sm-4 pb-2" key={item.slug}>
-                            <ItemPreview item={item} />
-                        </div>
-                    );
-                })}
-            </div>
-            <ListPagination
-                pager={props.pager}
-                itemsCount={props.itemsCount}
-                currentPage={props.currentPage}
-            />
-        </div>
+  if (props.titleSearchTerm && props.titleSearchTerm.length >= 3) {
+    items = items.filter((item) =>
+      item.title
+        .toLowerCase()
+        .includes(props.titleSearchTerm.toLowerCase().trim())
     );
+  }
+
+  return (
+    <div className="container py-2">
+      <div className="row">
+        {items.map((item) => {
+          return (
+            <div className="col-sm-4 pb-2" key={item.slug}>
+              <ItemPreview item={item} />
+            </div>
+          );
+        })}
+      </div>
+      <ListPagination
+        pager={props.pager}
+        itemsCount={props.itemsCount}
+        currentPage={props.currentPage}
+      />
+    </div>
+  );
 };
 export default ItemList;
